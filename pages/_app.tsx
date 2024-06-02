310cc39e08746792d445b4ed53fc23acd6ca9f70
Hijikata1869 <liv.knk@gmail.com>
2024-06-02 18:17:56 +0900

    アプリケーション全体をCurrentUserContextProviderでラップ#11

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SearchContextProvider } from "@/context/SearchContext";
+import { CurrentUserContextProvider } from "@/context/CurrentUserContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SearchContextProvider>
-      <Component {...pageProps} />
-    </SearchContextProvider>
+    <CurrentUserContextProvider>
+      <SearchContextProvider>
+        <Component {...pageProps} />
+      </SearchContextProvider>
+    </CurrentUserContextProvider>
   );
 }
