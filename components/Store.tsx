74fb310b625550b292a54dc52aa531272a701fa2
Hijikata1869 <liv.knk@gmail.com>
2024-07-09 20:49:37 +0900

    ログインしていればお気に入り、お気に入り解除ボタンを店舗ページに表示#28

--- components/Store.tsx
+++ components/Store.tsx
@@ -1,19 +1,37 @@
-import { useEffect, useState, memo } from "react";
+import { useEffect, useState, memo, useContext } from "react";
 import { useRouter } from "next/router";
+import Cookie from "universal-cookie";
+
+import { CurrentUserContext } from "@/context/CurrentUserContext";
 
 import { fetchStore } from "@/lib/stores";
-import { StoreData } from "@/types/store";
+import {
+  createFavorite,
+  fetchCurrentUserFavoriteStores,
+  deleteFavorite,
+} from "@/lib/favorites";
+import { StoreData, StoreDatas } from "@/types/store";
 
 import { CrowdednessTable } from "./CrowdednessTable";
 import { CrowdednessForm } from "./CrowdednessForm";
 import { LatestCrowdednessReviews } from "./LatestCrowdednessReviews";
 
+const cookie = new Cookie();
+
 export const Store: React.FC = memo(() => {
   const router = useRouter();
   const storeId = parseInt(router.query.id as string);
+  const accessToken = cookie.get("access_token");
+
+  const currentUserContext = useContext(CurrentUserContext);
+  const { isLogin } = currentUserContext;
 
   const [store, setStore] = useState<StoreData>();
   const [isCrowdedness, setIsCrowdedness] = useState<boolean>(true);
+  const [favoriteStores, setFavoriteStores] = useState<StoreDatas | undefined>(
+    undefined
+  );
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
   useEffect(() => {
     if (!isNaN(storeId)) {
@@ -32,15 +50,91 @@ export const Store: React.FC = memo(() => {
     // eslint-disable-next-line
   }, [router.query.id]);
 
+  useEffect(() => {
+    setIsDisabled(true);
+    isLogin &&
+      fetchCurrentUserFavoriteStores(accessToken)
+        .then(async (res) => {
+          const data = await res.favorite_stores;
+          setFavoriteStores(data);
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .finally(() => {
+          setIsDisabled(false);
+        });
+  }, [isLogin, accessToken]);
+
   const onClickToggle = () => {
     setIsCrowdedness(!isCrowdedness);
+  };
+
+  const onClickCreateFavorite = () => {
+    setIsDisabled(true);
+    createFavorite(storeId, accessToken)
+      .then(async (res) => {
+        const data = await res.favorite_stores;
+        setFavoriteStores(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsDisabled(false);
+      });
+  };
+
+  const onClickDeleteFavorite = () => {
+    setIsDisabled(true);
+    deleteFavorite(storeId, accessToken)
+      .then(async (res) => {
+        const data = await res.favorite_stores;
+        setFavoriteStores(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .finally(() => {
+        setIsDisabled(false);
+      });
+  };
+
+  const isStoreInList = (
+    storeList: StoreDatas | undefined,
+    storeId: number
+  ) => {
+    return storeList?.some((store) => store.id === storeId);
   };
 
   return (
     <div className="w-full px-40 mb-20">
       <div className="flex flex-col bg-white rounded-lg p-20 mt-20">
         <div>
-          <h2 className="font-bold text-4xl mb-3">{store?.name}</h2>
+          <div className="flex justify-between items-center">
+            <h2 className="font-bold text-3xl mb-3">{store?.name}</h2>
+            {isLogin && (
+              <>
+                {isStoreInList(favoriteStores, storeId) ? (
+                  <button
+                    className="px-4 py-2 bg-white rounded text-sky-500 hover:bg-sky-100 transition outline outline-sky-200 font-bold ml-5"
+                    onClick={onClickDeleteFavorite}
+                    disabled={isDisabled}
+                  >
+                    お気に入り解除
+                  </button>
+                ) : (
+                  <button
+                    className="px-4 py-2 bg-sky-500 rounded text-gray-50 hover:bg-sky-700 transition font-bold ml-5"
+                    onClick={onClickCreateFavorite}
+                    disabled={isDisabled}
+                  >
+                    お気に入りに追加
+                  </button>
+                )}
+              </>
+            )}
+          </div>
           <p className="text-gray-400 ">{store?.address}</p>
           <div className="flex justify-center mx-4 mt-10">
             {isCrowdedness ? (
---
+++ lib/favorites.ts
@@ -0,0 +1,57 @@
+export const createFavorite = async (storeId: number, token: string) => {
+  return await fetch(
+    `${process.env.NEXT_PUBLIC_RAILSAPI_URL}stores/${storeId}/favorites`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    }
+  ).then(async (res) => {
+    if (res.status === 200) {
+      const data = await res.json();
+      return data;
+    } else {
+      throw "お気に入り登録できませんでした";
+    }
+  });
+};
+
+export const fetchCurrentUserFavoriteStores = async (token: string) => {
+  return await fetch(
+    `${process.env.NEXT_PUBLIC_RAILSAPI_URL}current_user/favorite_stores`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    }
+  ).then(async (res) => {
+    if (res.status == 200) {
+      const data = await res.json();
+      return data;
+    } else throw "取得失敗";
+  });
+};
+
+export const deleteFavorite = async (storeId: number, token: string) => {
+  return await fetch(
+    `${process.env.NEXT_PUBLIC_RAILSAPI_URL}stores/${storeId}/favorites`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    }
+  ).then(async (res) => {
+    if (res.status === 200) {
+      const data = await res.json();
+      return data;
+    } else {
+      throw "削除失敗";
+    }
+  });
+};
