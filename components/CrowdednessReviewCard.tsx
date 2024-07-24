cf9494cd7e2d1c62f3db77b52de9211187a8ba50
Hijikata1869 <liv.knk@gmail.com>
2024-07-24 17:07:09 +0900

    トップページをレスポンシブ化#36

--- components/CrowdednessReviewCard.tsx
+++ components/CrowdednessReviewCard.tsx
@@ -19,38 +19,60 @@ export const CrowdednessReviewCard: React.FC<CrowdednessReviewCardProps> = memo(
       }
     };
     return (
-      <div className="px-20 w-full">
+      <div className="lg:px-20 md:px-10 w-full">
         {reviews?.map((review) => (
           <div
             key={review.id}
             className="bg-white px-8 py-4 mb-10 rounded-lg shadow-md flex flex-col"
           >
-            <div className="flex justify-between items-center mb-2">
-              <div>
-                <p className="text-gray-500 text-xs">{`${review.created_at} ${review.day_of_week} ${review.time}の訪問`}</p>
+            <div className="hidden md:block">
+              <div className="flex justify-between items-center mb-2">
+                <div>
+                  <p className="text-gray-500 text-xs">{`${review.created_at} ${review.day_of_week} ${review.time}の訪問`}</p>
+                </div>
+                <div
+                  className={`px-2 py-1 rounded ${changeBackgroundColor(
+                    review.level
+                  )}`}
+                >
+                  <p className={`text-sm text-white font-bold`}>
+                    混み具合：{review.level}
+                  </p>
+                </div>
               </div>
-              <div
-                className={`px-2 py-1 rounded ${changeBackgroundColor(
-                  review.level
-                )}`}
-              >
-                <p className={`text-sm text-white font-bold`}>{review.level}</p>
+            </div>
+            <div className="md:hidden mb-2">
+              <div className="flex flex-col justify-start items-start">
+                <div>
+                  <p className="text-gray-500 text-xs mb-2">{`${review.created_at} ${review.day_of_week} ${review.time}の訪問`}</p>
+                </div>
+                <div
+                  className={`px-2 py-1 rounded ${changeBackgroundColor(
+                    review.level
+                  )}`}
+                >
+                  <p className={`text-sm text-white font-bold`}>
+                    混み具合：{`${review.level}`}
+                  </p>
+                </div>
               </div>
             </div>
             <div>
               <Link
                 href={`/stores/${review.store_id}`}
-                className="font-bold text-lg text-cyan-600"
+                className="font-bold md:text-lg text-sm text-cyan-600"
               >
                 {review.store_name}
               </Link>
             </div>
             {review.memo?.length !== 0 && (
               <div className="mt-2">
-                <p className="text-gray-900">{review.memo}</p>
+                <p className="text-gray-900 text-sm md:text-base">
+                  {review.memo}
+                </p>
               </div>
             )}
-            <div className="flex items-center justify-end mt-4">
+            <div className="flex items-center md:justify-end justify-start mt-4">
               <div className="flex items-center">
                 <div className="relative w-10 h-10">
                   <Image
