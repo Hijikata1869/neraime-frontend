6cc303011c3dff29981ab3a89c535ef4ad60161a
Hijikata1869 <liv.knk@gmail.com>
2025-04-17 23:06:54 +0900

    SuccessAlertコンポーネントのテストを作成#58

---
+++ __test__/components/SuccessAlert.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { SuccessAlert } from "@/components/SuccessAlert";
+import NotificationContext from "@/context/notificationContext";
+import { mock } from "node:test";
+
+const mockFn = jest.fn();
+
+test("SuccessAlertが表示されること", () => {
+    render(<NotificationContext.Provider value={{
+        notification: "success",
+        notificationText: "成功しました",
+        success: mockFn,
+        error: mockFn,
+        clear: mockFn
+    }}>
+        <SuccessAlert />
+    </NotificationContext.Provider>)
+    expect(screen.getByText("成功しました")).toBeInTheDocument();
+})
