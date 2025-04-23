ca248eba47d7f83de352c79d7083c66b7499bac0
Hijikata1869 <liv.knk@gmail.com>
2025-04-23 20:57:58 +0900

    Footerコンポーネントにテストを追加#58

--- __test__/components/Footer.test.tsx
+++ __test__/components/Footer.test.tsx
@@ -15,4 +15,20 @@ test("フッターが表示されていること", () => {
     expect(screen.getByText("プライバシーポリシー")).toBeInTheDocument();
     expect(screen.getByText("お問い合わせ")).toBeInTheDocument();
     expect(screen.getByText("NERAIMEについて")).toBeInTheDocument();
-})
+});
+
+test("フッターのリンクが正しく遷移すること", () => {
+    render(<Footer />);
+    const privacyLink = screen.getByRole("link", {
+        name: "プライバシーポリシー"
+    })
+    const contactLink = screen.getByRole("link", {
+        name: "お問い合わせ"
+    })
+    const aboutLink = screen.getByRole("link", {
+        name: "NERAIMEについて"
+    })
+    expect(privacyLink).toHaveAttribute("href", "/privacy");
+    expect(contactLink).toHaveAttribute("href", "/contact");
+    expect(aboutLink).toHaveAttribute("href", "/about");
+});
