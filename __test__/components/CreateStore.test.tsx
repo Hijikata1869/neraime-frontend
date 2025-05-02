8dd8b856cc32b74c5b85c57eacd2c7cb96e73018
Hijikata1869 <liv.knk@gmail.com>
2025-05-02 13:06:28 +0900

    店舗新規登録ページのテストを作成#58

---
+++ __test__/components/CreateStore.test.tsx
@@ -0,0 +1,66 @@
+import { render, screen } from "@testing-library/react";
+import { CreateStore } from "@/components/CreateStore";
+import { SearchContext } from "@/context/SearchContext";
+import { CurrentUserContext } from "@/context/CurrentUserContext";
+type CurrentUserObj = {
+    id: number;
+    nickname: string;
+    email: string;
+    password_digest: string;
+    self_introduction?: string;
+    created_at: string;
+    updated_at: string;
+}
+
+const testCurrentUser = {
+    id: 1,
+    nickname: "test_user",
+    email: "test_user@example.com",
+    password_digest: "test_pass",
+    created_at: "yyyy-mm-dd",
+    updated_at: "yyyy-mm-dd",
+}
+
+jest.mock("next/router", () => ({
+    useRouter: jest.fn(),
+}))
+
+const mockFn = jest.fn();
+
+const testCandidateStore = [
+    {
+        place_id: "store_id", 
+        name: "store_name", 
+        formatted_address: "store_address", 
+        types: ["spa"]
+    }
+];
+
+const testSelectedCandidate = {
+    name: "store_name",
+    address: "store_address",
+}
+
+test("店舗の新規登録ページが表示されること", () => {
+    render(
+        <CurrentUserContext.Provider value={{
+            currentUser: testCurrentUser,
+            setCurrentUser: mockFn,
+            isLogin: true,
+            setIsLogin: mockFn
+        }}>
+            <SearchContext.Provider value={{
+                candidates: testCandidateStore,
+                setCandidates: mockFn,
+                selectedCandidate: testSelectedCandidate,
+                setSelectedCandidate: mockFn
+
+            }}>
+                <CreateStore />
+            </SearchContext.Provider>
+        </CurrentUserContext.Provider>
+        );
+        expect(screen.getByText(/store_name/)).toBeInTheDocument();
+        expect(screen.getByText(/store_address/)).toBeInTheDocument();
+        expect(screen.getByRole("button", { name: "登録する" })).toBeInTheDocument();
+});
