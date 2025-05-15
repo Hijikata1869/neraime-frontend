9e0378a852523b4d938c025238affac0cfc8535e
Hijikata1869 <liv.knk@gmail.com>
2025-05-15 21:52:04 +0900

    新規ユーザー作成のE2Eテストを作成#58

--- e2e/SignUp.spec.ts
+++ e2e/SignUp.spec.ts
@@ -1,5 +1,25 @@
 import { test, expect } from '@playwright/test';
 
 test("新規ユーザーを作成できること",async ({ page }) => {
-    await page.goto("http://localhost:3000/signup/");
+
+    const timestamp = Date.now();
+    const email = `test_user_${timestamp}@example.com`;
+
+    await page.goto("http://localhost:3200/sign-up/");
+
+    await page.getByRole("textbox", {
+        name: "ニックネーム"
+    }).fill("test_user");
+
+    await page.getByRole("textbox", {
+        name: "Eメール"
+    }).fill(email);
+
+    await page.getByRole('textbox', { name: 'パスワード', exact: true }).fill("test_user_pass");
+    await page.getByRole('textbox', { name: 'パスワード（確認用）' }).fill("test_user_pass");
+
+    await page.getByRole('button', { name: '新規登録' }).nth(2).click();
+
+    
+    await expect(page.getByText("登録しました")).toBeVisible();
 });
